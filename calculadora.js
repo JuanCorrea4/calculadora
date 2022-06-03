8fd8ebdaba649bdd2c2437f86b0c093bd127837e
Juan Correa <102707755+JuanCorrea4@users.noreply.github.com>
2022-06-03 18:52:16 -0500

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -4,6 +4,13 @@ let multiplicacion = document.getElementById("multiplicacion").ariaDisabled ="tr
 let division = document.getElementById("division").ariaDisabled ="true"
 function numeros (numero){
   document.getElementById("pantalla").value+=numero
+    let operacion = document.getElementById("pantalla").value 
+      operacion = operacion.chatAt(operacion.length - 1)
+        if((operacion == "+" || operacion == "-" || operacion == "*" || operacion == "/") && (numero == "+" || numero == "-" || numero == "*" || numero == "/")){
+        }
+          else {
+            document.getElementById("pantalla").value += numero
+          }
   }
     function borr (){
       document.getElementById("pantalla").value=""
